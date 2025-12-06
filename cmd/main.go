9ee984c6917b67f0293cfc980a34ff603f13f27b
whotterre <ijeddy5.0@gmail.com>
2025-12-06 09:58:01 +0100

    feat: integrate worker with main Gin server

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"log"
 	"whotterre/img_service/internal/config"
+	"whotterre/img_service/internal/initializers"
+	"whotterre/img_service/internal/repositories"
 	"whotterre/img_service/internal/routes"
+	"whotterre/img_service/internal/workers"
 
 	"github.com/gin-gonic/gin"
+	"github.com/hibiken/asynq"
 )
 
 func main() {
@@ -16,7 +20,32 @@ func main() {
 
 	app := gin.Default()
 
-	routes.SetupRoutes(app)
+	err := initializers.ConnectToDB()
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+	db := initializers.GetDB()
+
+	redisOpt := asynq.RedisClientOpt{
+		Addr:     config.AppConfig.RedisAddr,
+		Password: config.AppConfig.RedisPassword,
+		DB:       config.AppConfig.RedisDB,
+	}
+
+	taskDistributor := workers.NewTaskDistributor(redisOpt)
+
+	routes.SetupRoutes(app, db, taskDistributor)
+
+	jobRepo := repositories.NewJobRepository(db)
+	imageRepo := repositories.NewUploadRepository(db)
+	taskProcessor := workers.NewRedisTaskProcessor(redisOpt, jobRepo, imageRepo)
+
+	go func() {
+		log.Println("Starting async task processor...")
+		if err := taskProcessor.Start(); err != nil {
+			log.Fatalf("Failed to start task processor: %v", err)
+		}
+	}()
 
 	port := ":" + config.AppConfig.Port
 	log.Printf("Starting image processing service on port %s", config.AppConfig.Port)
